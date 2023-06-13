cb8d35938c87b8071ca8707cf5e0f1a9dffedcb6
jhall7620 <jhall7620@gmail.com>
2023-06-12 22:44:55 -0400

    Basic Site

---
+++ app.js
@@ -0,0 +1,8 @@
+const board = document.querySelector("#board")
+const player = document.querySelector('#player')
+const infoDisplay = document.querySelector("#info-display")
+
+const width=8
+const startPieces = [
+    
+]
